ed2badb5a678a204780ca6e45f6dac3dc5093df7
nanophate <nanophate@zoho.com>
2014-01-23 21:44:05 +0900

    first commit

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './site'
+tun Sinatra::Application
---
+++ site.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'instagram'
+
+enable :sessions
+CALLBACK_URL = 'http://instag.herokuapp.com/oauth/callback'
+
+Instagram.configure do |config|
+ config.client_id = ENV['client_id']
+ config.client_secret = ENV['client_secret']
+end
+
+get '/' do
+ "<a href='/oauth/connect'>Connect with Instagram</a>"
+end
+
+get '/oauth/connect' do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+get '/oauth/callback' do
+  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  session[:access_token] = response.access_token
+  redirect '/feed'
+end
+
+get "/feed" do
+  client = Instagram.client(:access_token => session[:access_token])
+  @user = client.user.username
+  @recent = client.user_recent_media
+  erb :index
+end
+
+
+
